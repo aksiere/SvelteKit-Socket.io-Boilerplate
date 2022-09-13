62331b01ca221e65a45e0dfcf7098426c5d0dc13
Helge <33295331+aksiere@users.noreply.github.com>
2022-09-13 04:34:29 +0300

    sveltekit updates

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,8 @@
 <script>
 	import { socket } from '$lib/socket'
-
-	const testSocket = () => {
-		socket.emit('test')
-		console.log(1)
-	}
 </script>
 
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 
-<button on:click={testSocket}>send test socket</button>
+<button on:click={() => { socket.emit('test') }}>send test socket</button>
